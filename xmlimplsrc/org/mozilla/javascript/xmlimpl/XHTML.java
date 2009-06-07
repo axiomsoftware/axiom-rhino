aafdabb2aa16c02ee504c705c3b45dd15a3a18b7
Nick <nicholas.j.campbell@gmail.com>
2009-06-07 14:24:45 -0400

    Default toString took out script tags that were root nodes. Also, &amp; were changed to &.

--- xmlimplsrc/org/mozilla/javascript/xmlimpl/XHTML.java
+++ xmlimplsrc/org/mozilla/javascript/xmlimpl/XHTML.java
@@ -1,5 +1,7 @@
 package org.mozilla.javascript.xmlimpl;
 
+import java.io.StringReader;
+
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Function;
 import org.mozilla.javascript.IdFunctionObject;
@@ -7,6 +9,7 @@ import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.xml.XMLObject;
 import org.mozilla.javascript.xmlimpl.XMLLibImpl;
 import org.mozilla.javascript.xmlimpl.XMLList;
+import org.xml.sax.InputSource;
 
 public class XHTML extends XMLList implements Function{
 	private static final long serialVersionUID = 1L;
@@ -41,8 +44,13 @@ public class XHTML extends XMLList implements Function{
         return lib.getXHTMLProcessor();
     }
 	
-    String toXMLString() {
-        StringBuffer sb = new StringBuffer();
+	public InputSource toInputSource() {
+		InputSource in = new InputSource(new StringReader(this.toString()));
+		return in;
+	}
+	
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
         XmlProcessor processor = getProcessor();
         for (int i=0; i<length(); i++) {
             if (getProcessor().isPrettyPrinting() && i != 0) {
@@ -50,6 +58,10 @@ public class XHTML extends XMLList implements Function{
             }
             sb.append(_annos.item(i).ecmaToXMLString(processor));
         }
-        return sb.toString().replaceAll("&amp;", "&");
+        return sb.toString();
+	}
+	
+    String toXMLString() {
+        return this.toString().replaceAll("&amp;", "&");
     }
 }
